e501190195fd05d30572107f7bf86317d6b43046
Solaiman Mansyur <solaiman.mansyur@gmail.com>
2025-04-25 22:06:44 +0800

    skeleton UI

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3456;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello Student!');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/office', (req, res) => {
+  res.sendFile(__dirname + '/public/office.html');
+});
+
+app.get('/class', (req, res) => {
+  res.sendFile(__dirname + '/public/class.html');
 });
 
 app.listen(port, () => {
